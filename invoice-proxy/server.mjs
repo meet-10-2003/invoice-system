55dc9d3b3f1dc08dacff02da1e6ce5e6ff775597
meet-10-2003 <manmeetsingh102003@gmail.com>
2025-07-08 16:51:49 +0530

    serverm.js

--- invoice-proxy/server.mjs
+++ invoice-proxy/server.mjs
@@ -6,12 +6,7 @@ const PORT = 3001;
 
 const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby3j7JF8tSVzLKi5lorbaU9yO-jf9UbTtCyuJ_urzNEwsBOmsdf42Rs8n2JcfVx19uy/exec';
 
-app.use(cors({
-  origin: 'https://invoice-system-mu.vercel.app',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type'],
-}));
-
+app.use(cors());
 app.use(express.json());
 
 /**
