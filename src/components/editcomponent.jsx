f48c2efe2e47b02abc5c1c844b52ce0231f7b346
meet-10-2003 <manmeetsingh102003@gmail.com>
2025-07-09 18:21:57 +0530

    updated

--- src/components/editcomponent.jsx
+++ src/components/editcomponent.jsx
@@ -182,6 +182,7 @@ const [isPreviousFetched, setIsPreviousFetched] = useState(false);
   const [prevCartage, setPrevCartage] = useState('');
   const [prevPrevBalance, setPrevPrevBalance] = useState('');
   const [prevGrandTotal, setPrevGrandTotal] = useState('');
+  const [prevAmountPaid, setPrevAmountPaid] = useState('');
   const [prevCarryForward, setPrevCarryForward] = useState('');
 
 
