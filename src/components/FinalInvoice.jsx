a29319fbbddd507e4c0135078fc53222e730c4fe
meet-10-2003 <manmeetsingh102003@gmail.com>
2025-07-09 19:15:17 +0530

    updated

--- src/components/FinalInvoice.jsx
+++ src/components/FinalInvoice.jsx
@@ -506,7 +506,7 @@ const formatDate = (value) => {
             rel="noopener noreferrer"
             className="bg-purple-900 button-style text-white uppercase font-semibold px-4 py-2 rounded shadow hover:bg-purple-700"
           >
-            View Sheet
+            View Sheet hello
           </a>
 
           {isSaving ? (
